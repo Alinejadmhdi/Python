3fcb49b4d2064fe307c9cad1aafc6db3f2edfe9d
alinejadmhdi <alinejadmhdi@gmail.com>
2024-11-13 12:23:34 +0330

    written_the_main_functionality

--- NumberGuesser/src/main.py
+++ NumberGuesser/src/main.py
@@ -1,4 +1,21 @@
 import random
-
-a = random.randint(1, 100)
-input("Take a guess!:\n")
+# Creating a random number
+answer = random.randint(1, 100)
+while True:
+    if userScore == 0:
+        print("Game Over")
+    guess = input("Take a guess! or enter q to exit:\n")
+    if guess.lower() == 'q':
+        print("Goodbye!")
+        break
+    else:
+        guess = int(guess)
+    userScore = 100
+    if guess == answer:
+        print(f"You won, You've scored {userScore} out of 100!")
+    elif guess < answer:
+        print("Higher!")
+        userScore -= 5
+    else:
+        print("Lower!")
+        userScore -= 5
