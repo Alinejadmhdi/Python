c16f1e9ef1d91f7dee398898422a9a2398315920
alinejadmhdi <alinejadmhdi@gmail.com>
2024-11-16 14:48:47 +0330

    numgusser fix

--- NumberGuesser/src/main.py
+++ NumberGuesser/src/main.py
@@ -1,21 +1,38 @@
 import random
-# Creating a random number
-answer = random.randint(1, 100)
-while True:
-    if userScore == 0:
-        print("Game Over")
-    guess = input("Take a guess! or enter q to exit:\n")
+
+def validate_guess(guess):
+    if not guess.isdigit():
+        print("Please enter a valid number!")
     if guess.lower() == 'q':
         print("Goodbye!")
-        break
+        return
     else:
         guess = int(guess)
+        return guess
+
+def start_game():
+# Creating a random number
+    answer = random.randint(1, 100)
     userScore = 100
-    if guess == answer:
-        print(f"You won, You've scored {userScore} out of 100!")
-    elif guess < answer:
-        print("Higher!")
-        userScore -= 5
-    else:
-        print("Lower!")
-        userScore -= 5
+
+    while True:
+        guess = input("Take a guess between 1 and 100! or enter q to exit:\n")
+        guess = validate_guess(guess)
+        if not guess:
+            continue
+
+        if userScore == 0:
+            print("Game Over")
+            break
+        if guess == answer:
+            print(f"You won, You've scored {userScore} out of 100!")
+            break
+        elif guess < answer:
+            print("Higher!")
+            userScore -= 5
+        else:
+            print("Lower!")
+            userScore -= 5
+
+start_game()
+
